95c71a93413a3453a0f86bc7c84983355fbe68c6
Amit Patnaik <amitp1997@gmail.com>
2022-11-16 16:08:18 -0500

    some changes to faculty and student pages

--- src/app/create-faculty-page/create-faculty-page.component.ts
+++ src/app/create-faculty-page/create-faculty-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatInputModule } from '@angular/material/input'; 
 import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-faculty-page',
@@ -9,9 +10,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 })
 export class CreateFacultyPageComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+}
+
+engineeringItems: string[] = [ "Electrical and Computer Engineering Technology", "Chemical Engineering", "Electrical", "Mechanical", "Software", "Computer Science", "Industrial", "Interdisciplinary Business with Engineering Studies"];
+visualArtsItems: string[] = [ "Graph Design", "3D Modeling"];
+toggle = [false];
 }
