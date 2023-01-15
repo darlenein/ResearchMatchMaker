e23e03846a00cf273570fc11961f2f1a8b36640d
Darlene In <darlene.in93@gmail.com>
2023-01-15 00:09:47 -0500

    dynamically display filter match result list

--- src/app/student-folder/opportunity-board-page/opportunity-board-page.component.ts
+++ src/app/student-folder/opportunity-board-page/opportunity-board-page.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DepartmentModel } from 'src/app/models/department.model';
 import { ServiceDispatcher } from '../../ServiceDispatcher';
 
 @Component({
@@ -9,17 +11,43 @@ import { ServiceDispatcher } from '../../ServiceDispatcher';
   styleUrls: ['./opportunity-board-page.component.css']
 })
 export class OpportunityBoardPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  engineeringValue = new FormControl('');
+  humanitiesValue = new FormControl('');
+  politicalValue = new FormControl('');
+  scienceValue = new FormControl('');
+  nursingValue = new FormControl('');
+  businessValue = new FormControl('');
+  departments: DepartmentModel[];
+  toggle = [false];
+  engineeringItems: any[];
+  politicalScienceItems: any[];
+  humanitiesSocialScienceItems: any[];
+  businessItems: any[];
+  scienceItems: any[];
+  nursingItems: any[];
+  
+  constructor(public serviceDispatcher: ServiceDispatcher, private router: Router, private route: ActivatedRoute) { 
   }
 
-  engineeringItems: string[] = [ "Computer", "Chemical", "Electrical", "Mechanical", "Software"];
-  humanitiesSocialScienceItems: string[] = [ "Communication", "English", "Psychology", "Politcal Science", "History", "Digital Media"];
-  businessItems: string[] = [ "Accounting", "Economics", "Finance", "Marketing"];
-  scienceItems: string[]  = ["Biology", "Chemistry", "Environmental Science", "Physics", "Mathematics"]
-  nursingItems: string[] = ["Nursing"]
+  ngOnInit(): void {
+    this.serviceDispatcher.getAllDepartments().subscribe(response => { 
+      this.departments = response;
+      this.engineeringItems = this.getSubDepts(this.departments[0].id);
+      this.politicalScienceItems = this.getSubDepts(this.departments[1].id);
+      this.humanitiesSocialScienceItems = this.getSubDepts(this.departments[2].id);
+      this.businessItems = this.getSubDepts(this.departments[3].id);
+      this.scienceItems = this.getSubDepts(this.departments[4].id);
+      this.nursingItems = this.getSubDepts(this.departments[5].id);
+    });
+  }
 
-  toggle = [false];
+  getSubDepts(id:number): any{
+  let subdepartments: any[] = [];
+    this.serviceDispatcher.getAllSubdeptByDeptId(id).subscribe(items => {
+      items.map((item: any) => {
+        subdepartments.push(item);
+      });
+    });
+    return subdepartments;
+  }
 }
