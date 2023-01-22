530eb4fc3a356adc7cadcc391e37603ec0207357
amitp1997 <amitp1997@gmail.com>
2023-01-22 15:09:20 -0500

    pre-populating changes

--- src/app/student-folder/edit-student-profile-page/edit-student-profile-page.component.ts
+++ src/app/student-folder/edit-student-profile-page/edit-student-profile-page.component.ts
@@ -3,6 +3,10 @@ import { FormControl } from '@angular/forms';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { StudentModel } from 'src/app/models/student.model';
 import { ServiceDispatcher } from 'src/app/ServiceDispatcher';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-student-profile-page',
@@ -33,6 +37,7 @@ export class EditStudentProfilePageComponent implements OnInit {
   psuID: string;
   fileName = '';
   pfp: any;
+  student: any;
 
   constructor(private router: Router, public serviceDispatcher: ServiceDispatcher, private route: ActivatedRoute,) { 
     this.route.queryParams.subscribe(params => {
@@ -41,6 +46,27 @@ export class EditStudentProfilePageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.serviceDispatcher.getStudent('dxi5017').subscribe(response => {
+      this.student = response
+      this.firstName = new FormControl(this.student.firstName);
+      this.lastName = new FormControl(this.student.lastName);
+      this.email = new FormControl(this.student.email); 
+      this.gpa = new FormControl(this.student.gpa);
+      this.major = new FormControl(this.student.major);
+      this.minor = new FormControl(this.student.minor);
+      this.location = new FormControl(this.student.location);
+      this.gradMonth = new FormControl(this.student.gradMonth);
+      this.gradYear = new FormControl(this.student.gradYear);
+      this.skills = new FormControl(this.student.skills);
+      this.link1 = new FormControl(this.student.link1);
+      this.link2 = new FormControl(this.student.link2);
+      this.link3 = new FormControl(this.student.link3);
+      this.paid = new FormControl(this.student.paid);
+      this.nonpaid = new FormControl(this.student.nonpaid);
+      this.credit = new FormControl(this.student.credit);
+      this.interest = new FormControl(this.student.interest);
+      this.projects = new FormControl(this.student.projects);
+    });
   }
   engineeringItems: string[] = [ "Computer", "Chemical", "Electrical", "Mechanical", "Software"];
   humanitiesSocialScienceItems: string[] = [ "Communication", "English", "Psychology", "Politcal Science", "History", "Digital Media"];
@@ -105,3 +131,6 @@ export class EditStudentProfilePageComponent implements OnInit {
     this.router.navigate(['/view-student-profile'], navigationExtras);
   }
 }
+
+  
+
