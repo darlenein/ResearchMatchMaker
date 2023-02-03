691f28eed045ff3beed1e48a20974b163340ec8c
Kole <doyne.kole@yahoo.com>
2023-02-02 20:14:01 -0500

    Modified routing to not use SSO for now

--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 @Component({
   selector: 'app-auth-callback',
   templateUrl: './auth-callback.component.html',
   styleUrls: ['./auth-callback.component.css']
 })
 export class AuthCallbackComponent implements OnInit {
-
-  constructor(private authService: AuthService) { }
+  
+  constructor(private router: Router, private route:ActivatedRoute, private authService: AuthService) {
+    
+   }
 
   ngOnInit(): void {
       this.authService.completeAuthentication();
   }
+
+  goToProfilePage(){
+    
+
+    
+    this.router.navigate(['/create-student-page']);
+    // this.router.navigate(['/create-faculty-page']);
+  }
+
 
 }
