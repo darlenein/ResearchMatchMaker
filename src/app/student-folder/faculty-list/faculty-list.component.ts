8c5f969477ec89da663cf5cad75a6611509a752d
Kole <doyne.kole@yahoo.com>
2023-02-09 16:50:03 -0500

    Added search for faculty list

--- src/app/student-folder/faculty-list/faculty-list.component.ts
+++ src/app/student-folder/faculty-list/faculty-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { FacultyModel } from 'src/app/models/faculty.model';
 import { ServiceDispatcher } from 'src/app/ServiceDispatcher';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-faculty-list',
   templateUrl: './faculty-list.component.html',
@@ -15,6 +15,23 @@ export class FacultyListComponent implements OnInit {
   splitResearchInterest: any;
   psuID: string;
 
+
+  searchTerm = new FormControl('');
+  GPA = new FormControl('');
+  Major = new FormControl('');
+  Location = new FormControl('');
+  Incentive = new FormControl('');
+
+  toggle = [false];
+
+  filteredStudents: FacultyModel[];
+
+  // fm: FacultyFilterModel = {
+  //   faculty: [],
+  //   facultyFilterValue: [],
+  //   psuID: "",
+  //   keyword: ""
+  // };
   constructor(private router: Router, public serviceDispatcher: ServiceDispatcher, private route:ActivatedRoute) { 
     this.route.queryParams.subscribe(params => {
       this.psuID = params["psuID"];
@@ -55,4 +72,32 @@ export class FacultyListComponent implements OnInit {
     this.router.navigate(['/student-view-faculty'], navigationExtras)
   }
 
+  // ---------------Filter and Search Function-------------------------
+  FilterAndSearch(category:string, change:any){
+    // let fvm = new FacultyFilterValueModel();
+    // this.fm.student = this.faculty;
+    // this.fm.keyword = this.searchTerm.value!;
+
+    // if (change) {
+    // // add checked option to filtered value array
+    //   if(change.options[0].selected === true) {
+    //     fvm.categoryValue = category;
+    //     fvm.checkedValue = change.options[0].value;
+    //     this.fm.studentFilterValue.push(fvm);
+    //   }
+    //   else {
+    //     // remove the checked option from filtered value array
+    //     this.fm.studentFilterValue.forEach((element,index)=>{
+    //       if(element.checkedValue == change.options[0].value) this.fm.studentFilterValue.splice(index,1);
+    //     });
+    //   }
+    // }
+
+    // // get filtered research list
+    // this.serviceDispatcher.getFilteredAndSearchStudentList(this.fm).subscribe(response => {
+    //   this.filteredStudents = response;
+    //   this.splitInformationBySemicolon(this.filteredStudents);
+
+    // });
+  }
 }
