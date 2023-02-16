aa0ec24709003bbe8b106c29dba43ccfce98ab46
amitp1997 <amitp1997@gmail.com>
2023-02-16 14:04:17 -0500

    edit student changes

--- src/app/student-folder/edit-student-profile-page/edit-student-profile-page.component.ts
+++ src/app/student-folder/edit-student-profile-page/edit-student-profile-page.component.ts
@@ -15,17 +15,27 @@ import { FormGroup } from '@angular/forms';
 })
 export class EditStudentProfilePageComponent implements OnInit {
   
-  firstName = new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z -]*")]);;
-  lastName = new FormControl('');
-  email = new FormControl('');
+  skillForm = [
+    {
+      skill: '',
+      skillLevel: ''
+    }
+  ];
+
+  studentForm: FormGroup = new FormGroup('');
+  firstName = new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z -]*")]);fb: any;
+;
+  lastName = new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z -]*")]);
+  email = new FormControl('', [Validators.required, Validators.email]);
   phone = new FormControl(''); // dont need?
-  gpa = new FormControl('');
-  major = new FormControl('');
+  gpa = new FormControl('', [Validators.required, Validators.pattern('\\-?\\d*\\.?\\d{1,2}')]);
+  major = new FormControl('', [Validators.required]);
   minor = new FormControl('');
-  location = new FormControl('');
+  location = new FormControl('', [Validators.required]);
   gradMonth = new FormControl('');
   gradYear = new FormControl('');
-  skills = new FormControl('');
+  skillSet = new FormControl('');
+  skillSetLevel = new FormControl('');
   link1 = new FormControl('');
   link2 = new FormControl('');
   link3 = new FormControl('');
@@ -42,6 +52,14 @@ export class EditStudentProfilePageComponent implements OnInit {
   constructor(private router: Router, public serviceDispatcher: ServiceDispatcher, private route: ActivatedRoute,) { 
     this.route.queryParams.subscribe(params => {
       this.psuID = params["psuID"];
+
+      this.studentForm = this.fb.group({
+        paid: this.paid,
+        nonpaid: this.nonpaid,
+        credit: this.credit,
+      });
+  
+      this.studentForm.setErrors({required: true});
     });
   }
 
@@ -57,7 +75,8 @@ export class EditStudentProfilePageComponent implements OnInit {
       this.location = new FormControl(this.student.location);
       this.gradMonth = new FormControl(this.student.graduation_Month);
       this.gradYear = new FormControl(this.student.graduation_Year);
-      this.skills = new FormControl(this.student.skills);
+      this.skillSet = new FormControl(this.student.skillString);
+      this.skillSetLevel = new FormControl(this.student.skillLevelString);
       this.link1 = new FormControl(this.student.link1);
       this.link2 = new FormControl(this.student.link2);
       this.link3 = new FormControl(this.student.link3);
@@ -67,6 +86,14 @@ export class EditStudentProfilePageComponent implements OnInit {
       this.interest = new FormControl(this.student.research_Interest);
       this.projects = new FormControl(this.student.research_Project);
     });
+
+    this.studentForm.valueChanges.subscribe(newValue => {
+      if (newValue.paid === true || newValue.nonpaid === true || newValue.credit === true) {
+        this.studentForm.setErrors(null);
+      } else {
+        this.studentForm.setErrors({required: true});
+      }
+      });
   }
   engineeringItems: string[] = [ "Computer", "Chemical", "Electrical", "Mechanical", "Software"];
   humanitiesSocialScienceItems: string[] = [ "Communication", "English", "Psychology", "Politcal Science", "History", "Digital Media"];
@@ -86,6 +113,13 @@ export class EditStudentProfilePageComponent implements OnInit {
 
   goToProfileViewPage() {
     let sd = new StudentModel();
+    let skillString = "";
+    let skillLevelString = "";
+
+    this.skillForm.forEach(element => {
+      skillString = skillString.concat(element.skill + "; ");
+      skillLevelString = skillLevelString.concat(element.skillLevel + "; ");
+    });
     sd.student_Id = this.psuID;
     sd.first_Name = this.firstName.value!;
     sd.last_Name = this.lastName.value!;
@@ -96,7 +130,8 @@ export class EditStudentProfilePageComponent implements OnInit {
     sd.graduation_Month = this.gradMonth.value!;
     sd.graduation_Year = this.gradYear.value!;
     sd.preferLocation = this.location.value!;
-    sd.skills = this.skills.value!;
+    sd.skills = skillString;
+    sd.skillLevel = skillLevelString;
     sd.link1 = this.link1.value!;
     sd.link2 = this.link2.value!;
     sd.link3 = this.link3.value!;
@@ -120,17 +155,121 @@ export class EditStudentProfilePageComponent implements OnInit {
     }
     sd.research_Interest = this.interest.value!;
     sd.research_Project = this.projects.value!;
-    this.serviceDispatcher.editStudentProfile(sd).subscribe(response => { });
+    
 
     let navigationExtras: NavigationExtras = {
       queryParams: {
         "psuID": this.psuID
       }
     };
-    
+    if(!this.validate()){
+    this.serviceDispatcher.editStudentProfile(sd).subscribe(response => { });
     this.router.navigate(['/view-student-profile'], navigationExtras);
   }
 }
 
-  
+validate(): any {
+  let hasError = false;
 
+  if (this.firstName.invalid) {
+    this.firstName.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.lastName.invalid) {
+    this.lastName.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.email.invalid) {
+    this.email.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.gpa.invalid) {
+    this.gpa.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.major.invalid) {
+    this.major.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.location.invalid) {
+    this.location.markAsDirty();
+    hasError = true;
+  }
+
+  if (this.paid.value || this.nonpaid.value || this.credit.value) {
+    this.studentForm.setErrors(null);
+  } else {
+    this.studentForm.setErrors({required: true});
+    hasError = true;
+  }
+  return hasError;
+  
+}
+
+getEmailErrorMessage() {
+  if (this.email.hasError('required')) {
+    return 'You must enter an email';
+  }
+  return this.email.hasError('email') ? 'Not a valid email' : '';
+}
+
+getFirstNameErrorMessage() {
+  if (this.firstName.hasError('required')) {
+    return 'You must enter a first name';
+  }
+  return this.firstName.hasError('pattern') ? 'Not a valid first name' : '';
+}
+
+getLastNameErrorMessage() {
+  if (this.lastName.hasError('required')) {
+    return 'You must enter a last name';
+  }
+  return this.lastName.hasError('pattern') ? 'Not a valid last name' : '';
+}
+
+getGPAErrorMessage() {
+  if (this.gpa.hasError('required')) {
+    return 'You must enter a GPA';
+  }
+  return this.gpa.hasError('pattern') ? 'Not a valid GPA' : '';
+}
+
+getMajorErrorMessage() {
+  if (this.major.hasError('required')) {
+    return 'You must enter a Major';
+  }
+  return '';
+}
+
+getLocationErrorMessage() {
+  if (this.location.hasError('required')) {
+    return 'You must enter a Preferred Research Location';
+  }
+  return '';
+}
+
+getPaidErrorMessage() {
+  if (this.studentForm.invalid) {
+    return 'You must enter a Preferred Research Incentive';
+  }
+  return '';
+}
+
+// dynamically add more skills 
+addSkillField() {
+  this.skillForm.push({
+    skill: '',
+    skillLevel: ''
+  });
+}
+
+removeSkillField(index: number) {
+  this.skillForm.splice(index,1);
+}
+
+}
