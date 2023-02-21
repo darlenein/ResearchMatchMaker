bc014c98482d3b0359c89ef5b2ee130f3b3cf1fa
Darlene <darlene.in93@gmail.com>
2023-02-20 22:39:26 -0500

    make button functional

--- src/app/student-folder/match-research-to-student-page/match-research-to-student-page.component.ts
+++ src/app/student-folder/match-research-to-student-page/match-research-to-student-page.component.ts
@@ -84,15 +84,17 @@ goToProfile(id:string){
     let p = new ProgressModel();
     p.research_id = rID;
     p.student_id = this.psuID;
-    //this.serviceDispatcher.addResearchApplicant(p).subscribe(response => {});
-    this.openDialog('0ms', '0ms');
+    this.openDialog('0ms', '0ms', p);
   }
 
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string, p: ProgressModel): void {
     this.dialog.open(ResearchApplicantDialogComponent, {
       width: '500px',
       enterAnimationDuration,
       exitAnimationDuration,
+      data: {
+        applicationInfo: p
+      }
     });
   }
 
